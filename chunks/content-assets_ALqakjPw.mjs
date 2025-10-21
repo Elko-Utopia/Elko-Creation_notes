07dc69e669173016221d90907a034b69f8dbfb56
Elko-Utopia <elkoutopia@gmail.com>
2025-10-21 03:02:20 -0400

    del

--- chunks/content-assets_ALqakjPw.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as __ASTRO_IMAGE_IMPORT_1BIFs1 } from './blog-placeholder-1_DhpbVPFw.mjs';
-import { _ as __ASTRO_IMAGE_IMPORT_1uPW0b } from './blog-placeholder-2_CUkiHjwt.mjs';
-
-const contentAssets = new Map([["../../assets/blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject-alpha.md", __ASTRO_IMAGE_IMPORT_1BIFs1], ["../../assets/blog-placeholder-2.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fproject-beta.md", __ASTRO_IMAGE_IMPORT_1uPW0b]]);
-
-export { contentAssets as default };
