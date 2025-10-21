660284b105a987b370d905338bdcbc3f4d6287c7
Elko-Utopia <elkoutopia@gmail.com>
2025-10-21 02:58:23 -0400

    del

--- chunks/astro_DesbeBV_.mjs
+++
@@ -1,3 +0,0 @@
-import 'kleur/colors';
-import './astro/server_CN0qAJDK.mjs';
-import 'clsx';
--- chunks/consts_CkYcPHIr.mjs
+++
@@ -1,4 +0,0 @@
-const SITE_TITLE = "Elko's Creation Notes";
-const SITE_DESCRIPTION = "Welcome to my website!";
-
-export { SITE_DESCRIPTION as S, SITE_TITLE as a };
